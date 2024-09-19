65058afa211087586cd7d2a5eab06c040b9738d1
luxkatana <taseen.bibi@gmail.com>
2024-09-19 21:49:53 +0200

    Eerste routes gemaakt.

--- somtodayHW_todolist/somtodayHW_todolist/urls.py
+++ somtodayHW_todolist/somtodayHW_todolist/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("somtodayhw_api.urls.urlpatterns"))
+    path("api/", include("somtodayhw_api.urls")),
+    path("", include("todoapp.urls"))
 ]
--- somtodayHW_todolist/todoapp/views.py
+++ somtodayHW_todolist/todoapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import HttpRequest
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return redirect("/login")
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return render(request, "login.html")
