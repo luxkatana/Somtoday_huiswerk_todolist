ce97b19281b431c3413819d1700f0856880c7243
luxkatana Mohammed <taseen.bibi@gmail.com>
2024-09-25 16:44:13 +0000

    Redirect to /login if not authenticated

--- somtodayHW_todolist/HTMLserve/views.py
+++ somtodayHW_todolist/HTMLserve/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from api.models import Todo
 
 # Create your views here. 
 def index(request: HttpRequest) -> HttpResponse:
-    todos = Todo.objects.filter(owner=request.user).all()
+    if request.user.id is not None:
+        todos = Todo.objects.filter(owner=request.user).all()
+        return render(request, "index.html", {'todos': todos})
     
-    return render(request, "index.html", {'todos': todos})
+    return redirect("/login")
     # return redirect("/login")
 
 
