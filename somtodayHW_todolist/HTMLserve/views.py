eddd6fa7d19a8173b0cb02196422ae4f543bf01c
luxkatana Mohammed <taseen.bibi@gmail.com>
2024-09-25 14:52:34 +0000

    add index.html, / redirects to 'index.html'

--- somtodayHW_todolist/HTMLserve/views.py
+++ somtodayHW_todolist/HTMLserve/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
+class TestTodo:
+    def __init__(self, todo_id: int, text: str, current_state: bool=False):
+        self.id = todo_id
+        self.text = text
+        self.af = current_state
+
 # Create your views here. 
-def index(_: HttpRequest) -> HttpResponse:
-    return redirect("/login")
+def index(request: HttpRequest) -> HttpResponse:
+    todos = [
+        TestTodo(1, "Test 1", False),
+        TestTodo(2, "Test 2", False),
+        TestTodo(3, "Test 3", True),
+    ]
+    return render(request, "index.html", {'todos': todos})
+    # return redirect("/login")
 
 
 def login(request: HttpRequest) -> HttpResponse:
